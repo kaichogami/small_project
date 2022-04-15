1dc788b4722c150e833022f0817b491c95cba8fa
kaichogami <asishrocks95@gmail.com>
2022-04-15 17:19:20 +0530

    adding problem statement

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 '''
-The `data.json` file contains data about list of restuarants collected by Zomato. 
-The file has lot of data which I do not need.
-There are two tasks. One to transform the data as given in the 'sample_output.json' file.
-Second to write the documentation for all work involved to complete this task in 'Readme.md' file.
-
-Steps:
-* Clone the repository in to your local computer.
-* For the reposiory in your github account.
-* Create a new branch.
-* Make the required changes in the branch.
-* Commit your changes.
-* Push the changes to your forked reposiotry.
-* Create pull request in my repository(https://github.com/kaichogami/small_project).
-
-The documentation should have all the steps required to complete this task.
+Write the code here to transform the data.json file to the required format. Please store the new file under name 'output.json'
 '''

fa6da9f9d65bf77416ad2a9ac24b6ebb3c89b8d3
kaichogami <asishrocks95@gmail.com>
2022-04-16 10:56:54 +0530

    psedo code 1

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 '''
 Write the code here to transform the data.json file to the required format. Please store the new file under name 'output.json'
 '''
+import json
 
+file_name = 'data.json'
+with open(file_name) as f:
+    data_raw = json.load(f)
+
+data_raw[]
+
+def convert_data_json(data_raw):
+    new_data_dict = {}
+    for x in data_raw:
+        new_data_dict['restaurants'][0]['name'] = 'bbsr'
+    new_data_dict.to_json()
+    with open('output_json.json') as f:
+        f.write(new_data_dict)
+
+    return new_data_json
